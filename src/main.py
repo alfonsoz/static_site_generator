ea06dc5539ed6ed521c83265266a14a6be219e79
alfonsoz <fkeijzer@gmail.com>
2026-04-04 14:22:44 +0200

    add some simple print testcase to main for htmlnode

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import htmlnode
 import textnode
 
 
@@ -5,7 +6,15 @@ def main():
     test_node = textnode.TextNode(
         "This is some anchor text", textnode.TextType.LINK, "https://www.boot.dev"
     )
+
+    test_props = {
+        "href": "https://www.kagi.com",
+        "target": "_blank",
+    }
+    test_htmlnode = htmlnode.HTMLNode("hello", "42", test_node, test_props)
+
     print(test_node)
+    print(test_htmlnode)
 
 
 # if main, only run when it is executed directly, not when it is imported somehow
