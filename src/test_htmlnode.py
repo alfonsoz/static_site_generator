c4438c2f0bb4ddaa889c490cddcb30e145ffa924
alfonsoz <fkeijzer@gmail.com>
2026-04-08 18:52:43 +0200

    add unittests for parentnode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,7 +1,7 @@
 # unittests, run from test.sh in root folder
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 # split tests in multiple cases to always see all tests/know which one is wrong
@@ -56,6 +56,75 @@ class TestHTMLNode(unittest.TestCase):
     def test_leaf_multiple_props(self):
         node = LeafNode("img", "", {"src": "image.png", "alt": "a photo"})
         self.assertEqual(node.to_html(), '<img src="image.png" alt="a photo"></img>')
+
+    # --------------------- Test ParentNode ------------------#
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        # print(f"{parent_node.to_html()}")
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+    def test_to_html_with_more_grandchildren(self):
+        grandgrandchild_node = LeafNode("a", "smallest")
+        grandchild_node = ParentNode("b", [grandgrandchild_node])
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        # print(f"{parent_node.to_html()}")
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b><a>smallest</a></b></span></div>",
+        )
+
+    # multiple children at the same level
+    def test_to_html_multiple_children(self):
+        leaf1 = LeafNode("a", "smallest")
+        leaf2 = LeafNode("span", "hello")
+        leaf3 = LeafNode("div", "test")
+        parent_node = ParentNode("etc", [leaf1, leaf2, leaf3])
+        # print(f"{parent_node.to_html()}")
+        self.assertEqual(
+            parent_node.to_html(),
+            "<etc><a>smallest</a><span>hello</span><div>test</div></etc>",
+        )
+
+    # mixed children, parentnode whose children include both parent and childnode objects at the same level
+    def test_to_html_mixed_children(self):
+        child_leaf_node = LeafNode("a", "smallest")
+        mixed_node = ParentNode("span", [LeafNode(None, "plain"), child_leaf_node])
+        parent_node = ParentNode("div", [mixed_node])
+        # print(f"{parent_node.to_html()}")
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span>plain<a>smallest</a></span></div>",
+        )
+
+    # Leafnode with Nonetag as child
+    def test_to_html_leaf_none_child(self):
+        child_node = LeafNode(None, "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div>child</div>")
+
+    def test_parent_no_tag(self):
+        childnode = LeafNode("b", "hello")
+        parentnode = ParentNode(None, [childnode])
+        with self.assertRaises(ValueError) as e:
+            parentnode.to_html()
+        # print(e.exception)
+
+    def test_parent_no_child(self):
+        parentnode = ParentNode("a", None)
+        with self.assertRaises(ValueError) as e:
+            parentnode.to_html()
+        # print(e.exception)
 
 
 if __name__ == "__main__":
