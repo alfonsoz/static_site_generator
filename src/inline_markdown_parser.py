a12442335a25081eff69d4e10f6c58e45da9a646
alfonsoz <fkeijzer@gmail.com>
2026-04-15 20:30:58 +0200

    add extract markdown image and links/url to inline_markdown_parser

--- src/inline_markdown_parser.py
+++ src/inline_markdown_parser.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+import re
 
 
 # raw markdown to TextNodes
@@ -33,3 +34,19 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                         new_node = TextNode(sections[i], text_type)
                     result.append(new_node)
     return result
+
+
+# takes raw markdown text to extract images
+# returns list of tuples
+def extract_markdown_images(text):
+    # \!\[[^\[\]]*\] = every character between square brackets ![]
+    # \([^\(\)]*\) = every character between () and has no () inside
+    # r"" pythonic for treat the string as a raw string, no escape sequence or backslash needed
+    return re.findall(r"\!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+
+# takes raw markdown text and extracts links
+# return list of tuples with anchor text and url's
+def extract_markdown_links(text):
+    # (?<!!) = negative look behind, so there can be no ! before. (as that is an image)
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
